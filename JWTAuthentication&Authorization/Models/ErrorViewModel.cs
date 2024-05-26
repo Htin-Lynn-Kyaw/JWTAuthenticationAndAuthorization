c055257849f2825fec06e3c6a7777587f30371ba
William lynn <13thwerewolf@gmail.com>
2024-05-26 22:00:08 +0630

    Add project files.

---
+++ JWTAuthentication&Authorization/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JWTAuthentication_Authorization.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
